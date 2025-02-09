4e19b471dca03282c40afa9a9a8fc5ef73913785
mdbaird <matthewdbaird@gmail.com>
2025-02-09 01:43:38 +0000

    Deploying to gh-pages from @ mdbaird/mdbaird.github.io@a474057afddaf3bc7fbd0c6edad0ac762e019c1d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,27 +4,27 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
   },{id: "nav-curriculum-vitae",
-          title: "curriculum vitae",
+          title: "Curriculum Vitae",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
           },
         },{id: "nav-publications",
-          title: "publications",
+          title: "Publications",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
           },
         },{id: "nav-teaching",
-          title: "teaching",
+          title: "Teaching",
           description: "",
           section: "Navigation",
           handler: () => {
